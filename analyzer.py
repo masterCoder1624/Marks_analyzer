0bdf19feec6fc54358012a1d64a501447fa84b0b
priyanshu sharma <sharma.priyanshu1624@gmail.com>
2026-01-30 19:02:10 +0530

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -1,1 +1,16 @@
-
+def analyzer():
+  marks = list(map(int, input("enter the marks : ").split()))
+  #finding maximum and the minimum marks
+  maximum = marks[0]
+  minimum = marks[0]
+  
+  for m in marks:
+    if m < minimum:
+      minimum = m
+    if m > maximum:
+      maximum = m
+      
+    print("Highest marks:", highest)
+    print("Lowest marks:", lowest)
+    
+marks_analyzer()
